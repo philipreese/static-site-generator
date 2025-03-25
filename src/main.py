23b3c99e66b868ec00f850aa27a39e9ea6c84c5b
philipreese <pbreese42@gmail.com>
2025-03-25 14:07:52 -0400

    add copying from static to public

--- src/main.py
+++ src/main.py
@@ -1,27 +1,35 @@
+import os
+import shutil
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, TextType
 
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    # textNode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    # print(textNode)
-    # htmlNode = HTMLNode(props={
-    #   "href": "https://www.google.com",
-    #   "target": "_blank",
-    # })
-    # print(htmlNode)
-    # print(htmlNode.props_to_html())
-    node = ParentNode(
-        "p",
-        [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            LeafNode("i", "italic text"),
-            LeafNode(None, "Normal text"),
-        ],
-    )
+    recursive_copy(dir_path_static, dir_path_public)
 
-    print(node.to_html())
+
+def recursive_copy(source_dir, destination_dir):
+    if os.path.exists(destination_dir):
+        for item in os.listdir(destination_dir):
+            item_path = os.path.join(destination_dir, item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+
+    for item in os.listdir(source_dir):
+        source_item_path = os.path.join(source_dir, item)
+        destination_item_path = os.path.join(destination_dir, item)
+
+        if os.path.isfile(source_item_path):
+            shutil.copy(source_item_path, destination_item_path)
+        elif os.path.isdir(source_item_path):
+            if not os.path.exists(destination_item_path):
+                os.mkdir(destination_item_path)
+            recursive_copy(source_item_path, destination_item_path)
 
 
 main()
