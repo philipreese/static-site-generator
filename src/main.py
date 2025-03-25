d9e1f0f1f50128a08f1003c49b36e2396e156747
philipreese <pbreese42@gmail.com>
2025-03-25 15:33:00 -0400

    changes to allow for deployment to github pages

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
+import sys
 from webpage import generate_pages_recursive, recursive_copy
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
-    recursive_copy(dir_path_static, dir_path_public)
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    basepath = sys.argv[1]
+    print(basepath)
+    if basepath == "":
+        basepath = "/"
+    recursive_copy(dir_path_static, dir_path_docs)
+    generate_pages_recursive(basepath, dir_path_content, template_path, dir_path_docs)
 
 
 main()
--- src/webpage.py
+++ src/webpage.py
@@ -12,6 +12,8 @@ def recursive_copy(source_dir, destination_dir):
                 os.remove(item_path)
             elif os.path.isdir(item_path):
                 shutil.rmtree(item_path)
+    else:
+        os.mkdir(destination_dir)
 
     for item in os.listdir(source_dir):
         source_item_path = os.path.join(source_dir, item)
@@ -32,7 +34,7 @@ def extract_title(markdown):
     return h1_node.children[0].value
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f_p:
         markdown_content = f_p.read()
@@ -44,6 +46,8 @@ def generate_page(from_path, template_path, dest_path):
     replaced_text = template_text.replace("{{ Title }}", title).replace(
         "{{ Content }}", html_text
     )
+    replaced_text = replaced_text.replace('href="/', f"href='{basepath}")
+    replaced_text = replaced_text.replace('src="/', f"href='{basepath}")
 
     dest_path_dir = os.path.dirname(dest_path)
     if not os.path.exists(dest_path_dir):
@@ -53,17 +57,17 @@ def generate_page(from_path, template_path, dest_path):
         d_p.write(replaced_text)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(basepath, dir_path_content, template_path, dest_dir_path):
     for item in os.listdir(dir_path_content):
         source_item_path = os.path.join(dir_path_content, item)
         destination_item_path = os.path.join(dest_dir_path, item)
 
         if os.path.isfile(source_item_path):
             dest_file = os.path.splitext(destination_item_path)[0] + ".html"
-            generate_page(source_item_path, template_path, dest_file)
+            generate_page(basepath, source_item_path, template_path, dest_file)
         elif os.path.isdir(source_item_path):
             if not os.path.exists(destination_item_path):
                 os.mkdir(destination_item_path)
             generate_pages_recursive(
-                source_item_path, template_path, destination_item_path
+                basepath, source_item_path, template_path, destination_item_path
             )
